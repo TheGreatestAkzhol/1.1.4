c02d36e7647252b1c0411da62e31d7044d4bdd13
Akzhol <tomikkz54@gmail.com>
2022-10-15 02:43:59 +0600

    my second attempt 1.1.4

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -9,8 +9,14 @@ import org.hibernate.Session;
 import java.sql.*;
 public class Main {
     public static void main(String[] args) throws SQLException {
-        UserDaoHibernateImpl udhi = new UserDaoHibernateImpl();
-        udhi.cleanUsersTable();
+        UserService us = new UserServiceImpl();
+        us.createUsersTable();
+        us.saveUser("Akzhol","Serikbek",(byte)22);
+        us.saveUser("Miron","Yanovich",(byte)22);
+        us.saveUser("Jonny","Sins",(byte)22);
+        us.getAllUsers().stream().forEach((c) -> System.out.println(c));
+        us.cleanUsersTable();
+        us.dropUsersTable();
 
     }
 }
